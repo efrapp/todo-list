c6c93a006d3da9c652312b4bae9e4fd30a92f860
Efrain Pinto <efrapp@gmail.com>
2019-10-17 10:00:49 -0500

    Display project list and project content

--- dist/main.js
+++ dist/main.js
@@ -95,10 +95,68 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-const todo = Object(_todo__WEBPACK_IMPORTED_MODULE_0__["default"])({
-  title: 'First task',
-  description: 'Testing a new task',
-  dueDate: '20019/10/02',
+const dummyProject = () => {
+  const defaultProject = Object(_project__WEBPACK_IMPORTED_MODULE_1__["default"])({ title: 'Default Project' });
+  let todo;
+
+  for (let i = 0; i < 5; i += 1) {
+    todo = Object(_todo__WEBPACK_IMPORTED_MODULE_0__["default"])({
+      title: 'First task',
+      description: 'Testing a new task',
+      dueDate: '20019/10/02',
+    });
+    defaultProject.addTodo(todo);
+  }
+
+  return defaultProject;
+};
+
+const createTodoHTML = (todo) => {
+  const todoNode = document.createElement('div');
+  const titleNode = document.createElement('h2');
+  const descriptionNode = document.createElement('p');
+  const dueDateNode = document.createElement('p');
+  const priorityNode = document.createElement('p');
+
+  titleNode.textContent = todo.getTitle();
+  descriptionNode.textContent = todo.getDescription();
+  dueDateNode.textContent = todo.getDueDate();
+  priorityNode.textContent = todo.getPriority();
+
+  todoNode.appendChild(titleNode);
+  todoNode.appendChild(descriptionNode);
+  todoNode.appendChild(dueDateNode);
+  todoNode.appendChild(priorityNode);
+
+  return todoNode;
+};
+
+const displayProject = (project) => {
+  const projectTitleNode = document.createElement('h1');
+  const projectContainerNode = document.getElementById('project');
+  const todoContainerNode = document.createElement('div');
+  let todoNode;
+
+  project.getTodos().forEach((todo) => {
+    todoNode = createTodoHTML(todo);
+    todoContainerNode.appendChild(todoNode);
+  });
+
+  projectTitleNode.textContent = project.getTitle();
+  projectContainerNode.appendChild(projectTitleNode);
+  projectContainerNode.appendChild(todoContainerNode);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const defaultProject = dummyProject();
+  const defaultProjectLink = document.getElementById('default-project');
+
+  defaultProjectLink.textContent = defaultProject.getTitle();
+  displayProject(defaultProject);
+
+  defaultProjectLink.addEventListener('click', () => {
+    // displayProject(defaultProject);
+  });
 });
 
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,66 @@
 import Todo from './todo';
 import Project from './project';
 
-const todo = Todo({
-  title: 'First task',
-  description: 'Testing a new task',
-  dueDate: '20019/10/02',
+const dummyProject = () => {
+  const defaultProject = Project({ title: 'Default Project' });
+  let todo;
+
+  for (let i = 0; i < 5; i += 1) {
+    todo = Todo({
+      title: 'First task',
+      description: 'Testing a new task',
+      dueDate: '20019/10/02',
+    });
+    defaultProject.addTodo(todo);
+  }
+
+  return defaultProject;
+};
+
+const createTodoHTML = (todo) => {
+  const todoNode = document.createElement('div');
+  const titleNode = document.createElement('h2');
+  const descriptionNode = document.createElement('p');
+  const dueDateNode = document.createElement('p');
+  const priorityNode = document.createElement('p');
+
+  titleNode.textContent = todo.getTitle();
+  descriptionNode.textContent = todo.getDescription();
+  dueDateNode.textContent = todo.getDueDate();
+  priorityNode.textContent = todo.getPriority();
+
+  todoNode.appendChild(titleNode);
+  todoNode.appendChild(descriptionNode);
+  todoNode.appendChild(dueDateNode);
+  todoNode.appendChild(priorityNode);
+
+  return todoNode;
+};
+
+const displayProject = (project) => {
+  const projectTitleNode = document.createElement('h1');
+  const projectContainerNode = document.getElementById('project');
+  const todoContainerNode = document.createElement('div');
+  let todoNode;
+
+  project.getTodos().forEach((todo) => {
+    todoNode = createTodoHTML(todo);
+    todoContainerNode.appendChild(todoNode);
+  });
+
+  projectTitleNode.textContent = project.getTitle();
+  projectContainerNode.appendChild(projectTitleNode);
+  projectContainerNode.appendChild(todoContainerNode);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const defaultProject = dummyProject();
+  const defaultProjectLink = document.getElementById('default-project');
+
+  defaultProjectLink.textContent = defaultProject.getTitle();
+  displayProject(defaultProject);
+
+  defaultProjectLink.addEventListener('click', () => {
+    // displayProject(defaultProject);
+  });
 });
