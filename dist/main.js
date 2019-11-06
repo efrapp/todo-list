75c7a684be1792ee49eaed92db76d0a39c7d481b
Efrain Pinto <efrapp@gmail.com>
2019-11-06 08:01:09 -0500

    Remove methods to handle todos inside the project object

--- dist/main.js
+++ dist/main.js
@@ -594,18 +594,6 @@ ProjectLI.prototype.setTitle = function setTitle(t) {
   this.title = t || this.title;
 };
 
-ProjectLI.prototype.getTodos = function getTodos() { return this.todos; };
-
-ProjectLI.prototype.addTodo = function addTodo(todo) {
-  this.todos.push(todo);
-  return todo;
-};
-
-ProjectLI.prototype.removeTodo = function removeTodo(id) {
-  const todoIndex = this.todos.findIndex(todo => todo.id === parseInt(id, 10));
-  return this.todos.splice(todoIndex, 1);
-};
-
 /* harmony default export */ __webpack_exports__["default"] = (ProjectLI);
 
 
--- src/projectLI.js
+++ src/projectLI.js
@@ -6,16 +6,4 @@ ProjectLI.prototype.setTitle = function setTitle(t) {
   this.title = t || this.title;
 };
 
-ProjectLI.prototype.getTodos = function getTodos() { return this.todos; };
-
-ProjectLI.prototype.addTodo = function addTodo(todo) {
-  this.todos.push(todo);
-  return todo;
-};
-
-ProjectLI.prototype.removeTodo = function removeTodo(id) {
-  const todoIndex = this.todos.findIndex(todo => todo.id === parseInt(id, 10));
-  return this.todos.splice(todoIndex, 1);
-};
-
 export default ProjectLI;
